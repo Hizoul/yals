b2fc2bbc8cd53d3a7c13f7eae735e14586aeffd3
Matthias Müller-Brockhausen <git@mmb2.click>
2019-05-16 12:52:40 +0200

    :recycle: externalize to file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
-use std::fs::{read_dir,DirEntry};
+use std::fs::{read_dir,DirEntry,read_to_string};
+use std::path::{PathBuf};
 use std::env;
 use std::io::{Error};
 use rayon::prelude::*;
+
+fn check_dir(mut unwrapped_path: PathBuf, found_at_sign: bool) {
+  println!("Reading directory {}", unwrapped_path.display());
+  if !found_at_sign {
+    let last = unwrapped_path.components().last();
+    match last {
+      Some(c) => {
+        let name_end = c.as_os_str().to_string_lossy().into_owned();
+        let index = name_end.find("@");
+        match index {
+          Some(_) => {
+            check_dir(unwrapped_path.clone(), true);
+          },
+          None => {}
+        };
+      },
+      None => {}
+    };
+  }
+  unwrapped_path.push("package.json");
+  println!("Reading File {}", unwrapped_path.display());
+  let contents = read_to_string(unwrapped_path.as_path());
+  match contents {
+    Ok(v) => println!("file is ok"),
+    Err(_) => {}
+  }
+}
 
 fn main() {
     let to_scan: String;
@@ -13,10 +41,8 @@ fn main() {
     }
     println!("Scanning directory: {}", to_scan);
     let paths = read_dir(to_scan).unwrap();
-    let mut collected: Vec<Result<DirEntry, Error>> = paths.collect();
+    let collected: Vec<Result<DirEntry, Error>> = paths.collect();
     let parrallel_iterator = collected.into_par_iter();
-    parrallel_iterator.for_each(|path| {
-      println!("HI THIS IS {}", path.unwrap().path().display())
-    });
+    parrallel_iterator.for_each(|path| check_dir(path.unwrap().path(), false));
 
 }
