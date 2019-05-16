be191da18a8b6bd86140b9bfabc11f0248596b99
Matthias Müller-Brockhausen <git@mmb2.click>
2019-05-16 11:57:31 +0200

    :page_facing_up: parallel directory scanning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::{read_dir,DirEntry};
+use std::env;
+use std::io::{Error};
+use rayon::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let to_scan: String;
+    let arg: Vec<String> = env::args().collect();
+    if arg.len() > 1 {
+      to_scan = arg[1].to_owned();
+    } else {
+      to_scan = "./".to_owned();
+    }
+    println!("Scanning directory: {}", to_scan);
+    let paths = read_dir(to_scan).unwrap();
+    let mut collected: Vec<Result<DirEntry, Error>> = paths.collect();
+    let parrallel_iterator = collected.into_par_iter();
+    parrallel_iterator.for_each(|path| {
+      println!("HI THIS IS {}", path.unwrap().path().display())
+    });
+
 }
